37759b1bf52eb09fc52c88359164e45ed06a2c91
Jaadugartech <116385748+Jaadugartech@users.noreply.github.com>
2024-11-13 20:04:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 // Function to redirect to Google search
-    function searchGoogle() {
-      const query = document.getElementById("searchInput").value;
-      if (query) {
+function searchGoogle() {
+    const query = document.getElementById("searchInput").value;
+    if (query) {
         window.location.href = `https://www.google.com/search?q=${encodeURIComponent(query)}`;
-      }
     }
+}
 
-    // Trigger search on "Enter" key
-    document.getElementById("searchInput").addEventListener("keydown", function(event) {
-      if (event.key === "Enter") {
+// Trigger search on "Enter" key
+document.getElementById("searchInput").addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
         searchGoogle();
-      }
-    });
+    }
+});
